a8230b6210897ab619f6b112990648d41ea0cc1b
Kirill Vishenkov <vishkir1991@gmail.com>
2017-06-12 17:17:14 +0300

    console.log output to cli

--- src/init.js
+++ src/init.js
@@ -8,7 +8,7 @@ export default () => {
     .option('-f, --format [type]', 'output format')
     .arguments('<firstConfig> <secondConfig>')
     .action((firstConfig, secondConfig) => {
-      gendiff(firstConfig, secondConfig);
+      console.log(gendiff(firstConfig, secondConfig));
     })
     .parse(process.argv);
 };
